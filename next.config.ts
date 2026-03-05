c4876e42164238d9c74358c35280c4fd503b044a
Zidane Elfasya <111755264+zidaneelfasya@users.noreply.github.com>
2026-03-05 11:41:21 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'standalone', // Required for Docker deployment
 };
 
 export default nextConfig;
